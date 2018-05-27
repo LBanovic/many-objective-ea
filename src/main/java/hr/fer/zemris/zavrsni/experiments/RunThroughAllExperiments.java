7f2ac417ee1fc20edd8eac8855627b51732c6f3f
Luka Banović <luka.banovic@fer.hr>
2018-05-28 00:25:24 +0200

    experiment runner

---
+++ src/main/java/hr/fer/zemris/zavrsni/experiments/RunThroughAllExperiments.java
@@ -0,0 +1,38 @@
+package hr.fer.zemris.zavrsni.experiments;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static java.nio.file.FileVisitResult.*;
+
+public class RunThroughAllExperiments {
+
+    public static void main(String[] args) throws IOException {
+        Visitor v = new Visitor();
+        Files.walkFileTree(Paths.get("Experiments/SPEA2"), v);
+    }
+
+    private static class Visitor extends SimpleFileVisitor<Path>{
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            ExperimentParser.main(new String[]{file.toString()});
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            return CONTINUE;
+        }
+    }
+}
